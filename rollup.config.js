4234147b8e98d4cb1d44de49ec5f17fd36b35e1b
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-14 01:13:56 +0700

    Add project configs

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+import packageJson from './package.json' assert { type: 'json' };
+const name = packageJson.main.replace(/\.js$/, '');
+
+const bundle = (config) => ({
+  ...config,
+  input: 'src/index.ts',
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
+    external: ['react'],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [esbuild()],
+  }),
+  bundle({
+    external: ['react'],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+    plugins: [dts()],
+  }),
+];

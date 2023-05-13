f056a3dadd4c912578abd82432f72efba0851c40
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-14 01:14:04 +0700

    Add prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  printWidth: 100,
+};

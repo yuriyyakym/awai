19e32c14fad6646821237d203afb6588deb6760f
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 06:14:16 +0700

    Rename root export group -> family
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { composeState, cyclicFlow, flow } from './lib';
 export { default as action } from './action';
-export { default as group } from './group';
+export { default as family } from './family';
 export { default as state } from './state';
 export { default as useFlowValue } from './useFlowValue';
 export { default as useState } from './useFlowState';

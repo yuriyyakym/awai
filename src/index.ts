2ac03fc49818c19b6e0003701b12bd942d1013ca
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 03:26:13 +0700

    Remove useAction
    Obsolete since actions are callable
    
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export { composeState, cyclicFlow, flow } from './lib';
 export { default as Action } from './Action';
 export { default as FamilyState } from './FamilyState';
 export { default as State } from './State';
-export { default as useAction } from './useAction';
 export { default as useFlowValue } from './useFlowValue';
 export { default as useState } from './useFlowState';
--- src/useAction.ts
+++
@@ -1,8 +0,0 @@
-import Action from './Action';
-
-const useAction =
-  <Callback extends (...args: any) => any, A extends Action<Callback>>(action: A) =>
-  (...args: Parameters<Callback>) =>
-    action.call(...args);
-
-export default useAction;

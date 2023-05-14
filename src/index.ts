49c51484e79f1230303ad1434779387370917d23
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 06:06:32 +0700

    Export group from root file
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { composeState, cyclicFlow, flow } from './lib';
 export { default as action } from './action';
-export { default as FamilyState } from './FamilyState';
+export { default as group } from './group';
 export { default as state } from './state';
 export { default as useFlowValue } from './useFlowValue';
 export { default as useState } from './useFlowState';

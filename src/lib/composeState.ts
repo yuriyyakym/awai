80e81604cdd7b8e9ff1dbdfc9a7e8ec88dc356be
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 05:45:54 +0700

    Refactor composeState
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/lib/composeState.ts
+++ src/lib/composeState.ts
@@ -1,22 +1,23 @@
-import State from '../State';
+import state, { State } from '../state';
 
 const composeState = <T extends any[], U>(
   states: [...{ [K in keyof T]: State<T[K]> }],
   predicate: (...values: { [K in keyof T]: T[K] }) => U,
 ): State<U> & { dispose: () => void } => {
-  const values = states.map((state) => state.value) as { [K in keyof T]: T[K] };
-  const composedState = new State(predicate(...values));
+  const values = states.map((state) => state.get()) as { [K in keyof T]: T[K] };
+  const composedState = state(predicate(...values));
   let mounted = true;
 
   queueMicrotask(async () => {
     while (mounted) {
       /**
        * @todo Cleanup when disposed
+       * @todo Consider weakRefs
        * @see https://github.com/yuriyyakym/flow-store/issues/1
        */
-      await Promise.race(states.map((state) => state.changed));
-      const values = states.map((state) => state.value) as { [K in keyof T]: T[K] };
-      composedState.setValue(predicate(...values));
+      await Promise.race(states.map((state) => state.events.changed));
+      const values = states.map((state) => state.get()) as { [K in keyof T]: T[K] };
+      composedState.set(predicate(...values));
     }
   });
 

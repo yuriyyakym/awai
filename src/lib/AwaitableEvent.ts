274c3f2d5d483a414b56fa1fac41d15bc98ecb20
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 16:36:49 +0700

    Add AwaitableEvent filtering support
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/lib/AwaitableEvent.ts
+++ src/lib/AwaitableEvent.ts
@@ -1,5 +1,7 @@
 import { Resolver } from '../types';
 
+type FilterPredicate<T> = (value: T) => boolean;
+
 export default class AwaitableEvent<T> {
   private _awaiters: Resolver<T>[] = [];
 
@@ -11,5 +13,19 @@ export default class AwaitableEvent<T> {
     const awaiters = Array.from(this._awaiters);
     this._awaiters = [];
     awaiters.forEach((resolve) => resolve(value));
+  }
+
+  async filter(predicate: FilterPredicate<T>) {
+    return new Promise(async (resolve) => {
+      while (true) {
+        const newValue = await this;
+        const isApplicable = Boolean(predicate(newValue));
+
+        if (isApplicable) {
+          resolve(newValue);
+          return;
+        }
+      }
+    });
   }
 }

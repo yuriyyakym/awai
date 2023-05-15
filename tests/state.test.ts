25dbeb051f243d8c3c72918867ccc71095ee39e9
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 16:45:45 +0700

    Add state test case
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- tests/state.test.ts
+++ tests/state.test.ts
@@ -21,4 +21,20 @@ describe('state', () => {
     await greeting.set('Hello there');
     expect(await greeting).toEqual('Hello there');
   });
+
+  it('awaits for the right event', async () => {
+    const CODE = 1234;
+    const pin = state(CODE);
+
+    const correctPinPromise = pin.events.changed.filter((newPin) => newPin === CODE);
+
+    queueMicrotask(async () => {
+      await pin.set(4321);
+      await pin.set(1111);
+      await pin.set(CODE);
+      await pin.set(2222);
+    });
+
+    await expect(correctPinPromise).resolves.toBe(CODE);
+  });
 });

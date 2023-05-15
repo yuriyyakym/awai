adb5abb32ca4c756e5d451a34e493cdf2c5fd71e
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 20:34:34 +0700

    Add double-click test scenario
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

---
+++ tests/scenarios/double-click.test.ts
@@ -0,0 +1,30 @@
+import { action, delay, scenario, timeout } from '../../src';
+
+const DOUBLE_CLICK_TIMEOUT = 200;
+
+describe('Scenario: Double click', () => {
+  const click = action();
+  const doubleClick = jest.fn();
+
+  scenario(async () => {
+    await click.events.invoke;
+    await Promise.race([click.events.invoke, timeout(DOUBLE_CLICK_TIMEOUT)]);
+    await doubleClick();
+  });
+
+  it('catches double clicks properly', async () => {
+    await click();
+    await delay(DOUBLE_CLICK_TIMEOUT - 1);
+    await click();
+
+    await click();
+    await delay(DOUBLE_CLICK_TIMEOUT + 1);
+    await click();
+
+    await click();
+    await delay(DOUBLE_CLICK_TIMEOUT - 1);
+    await click();
+
+    expect(doubleClick).toBeCalledTimes(2);
+  });
+});

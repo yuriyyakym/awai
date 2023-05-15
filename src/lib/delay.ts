da24360b6620150dca7428e314fda233069a1da2
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 17:18:33 +0700

    Add delay util
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

---
+++ src/lib/delay.ts
@@ -0,0 +1,3 @@
+const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default delay;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 export { default as AwaitableEvent } from './AwaitableEvent';
 export { default as composeState } from './composeState';
+export { default as delay } from './delay';
 export { default as noop } from './noop';
 export { default as scenario } from './scenario';
 export { default as scenarioOnce } from './scenarioOnce';

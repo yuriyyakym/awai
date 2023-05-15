dc3b57cbd727af80c12609c1a0e4a717e4706265
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-15 07:20:30 +0700

    Fix family types
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/family/index.ts
+++ src/family/index.ts
@@ -1,1 +1,1 @@
-export { default } from './group';
+export { default } from './family';
--- src/family/types.ts
+++ src/family/types.ts
@@ -5,7 +5,7 @@ export type FamilyMap<T> = Record<string, State<T>>;
 export interface Family<T> {
   delete(key: string): Promise<T>;
   has(key: string): boolean;
-  getItem(key: string): T;
+  get(key: string): T;
   getItemState(key: string): State<T>;
   keys(): string[];
   set(key: string, value: T): void;

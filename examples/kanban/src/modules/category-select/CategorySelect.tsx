c6941da86bcc95cff8ffd20fed78fb00a18994df
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-16 15:45:04 +0700

    Update todo-list example CategorySelect component
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- examples/kanban/src/modules/category-select/CategorySelect.tsx
+++ examples/kanban/src/modules/category-select/CategorySelect.tsx
@@ -6,19 +6,26 @@ import { useCategories } from '../../state';
 
 interface Props {
   excludedCategoriesIds?: Category['id'][];
-  onChange: (category: Category) => void;
+  onSelect: (category: Category) => void;
 }
 
-const CategorySelect: FunctionComponent<Props> = ({ excludedCategoriesIds = [] }) => {
+const CategorySelect: FunctionComponent<Props> = ({ excludedCategoriesIds = [], onSelect }) => {
   const categories = useCategories();
   const filteredCategories = categories.filter(
     (category) => !excludedCategoriesIds.includes(category.id),
   );
 
+  const handleCategoryChange = (categoryId: Category['id'] | null) => {
+    const category = filteredCategories.find((category) => category.id === categoryId);
+    if (category) {
+      onSelect(category);
+    }
+  };
+
   return (
-    <DropdownButton align="end" title="Move" size="sm">
+    <DropdownButton align="end" title="Move" size="sm" onSelect={handleCategoryChange}>
       {filteredCategories.map((category) => (
-        <Dropdown.Item as={Button} key={category.id}>
+        <Dropdown.Item as={Button} eventKey={category.id} key={category.id}>
           {category.name}
         </Dropdown.Item>
       ))}

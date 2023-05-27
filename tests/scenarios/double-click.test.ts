f5686e96372b7d05964234ffd4966809b5abc1bb
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-27 11:18:30 +0700

    Refactor double-click scenario test
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- tests/scenarios/double-click.test.ts
+++ tests/scenarios/double-click.test.ts
@@ -1,30 +1,32 @@
-import { action, delay, scenario, rejectAfter } from '../../src';
+import { action, delay, rejectAfter, scenarioOnEvery } from '../../src';
 
 const DOUBLE_CLICK_TIMEOUT = 200;
 
 describe('Scenario: Double click', () => {
   const click = action();
-  const doubleClick = jest.fn();
+  const onDoubleClick = jest.fn();
 
-  scenario(async () => {
-    await click.events.invoked;
+  scenarioOnEvery(click.events.invoked, async () => {
     await Promise.race([click.events.invoked, rejectAfter(DOUBLE_CLICK_TIMEOUT)]);
-    await doubleClick();
+    await onDoubleClick();
   });
 
   it('catches double clicks properly', async () => {
     await click();
-    await delay(DOUBLE_CLICK_TIMEOUT - 1);
+    await delay(DOUBLE_CLICK_TIMEOUT - 10);
     await click();
 
     await click();
-    await delay(DOUBLE_CLICK_TIMEOUT + 1);
+    await delay(DOUBLE_CLICK_TIMEOUT + 10);
     await click();
 
     await click();
-    await delay(DOUBLE_CLICK_TIMEOUT - 1);
+    await delay(DOUBLE_CLICK_TIMEOUT - 10);
     await click();
 
-    expect(doubleClick).toBeCalledTimes(2);
+    // Wait for the next cycle, so that scenario is finished
+    await delay(0);
+
+    expect(onDoubleClick).toBeCalledTimes(2);
   });
 });

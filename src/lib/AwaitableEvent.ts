66263961eb0e3548f6ca5ff97106c301e56cab09
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-07-24 13:38:52 +0700

    Return Promise from `then` in AwaitableEvent

--- src/lib/AwaitableEvent.ts
+++ src/lib/AwaitableEvent.ts
@@ -2,11 +2,13 @@ import { Resolver } from '../types';
 
 type FilterPredicate<T> = (value: T) => boolean;
 
+// implements PromiseLike<T>
 export default class AwaitableEvent<T> {
   private _awaiters: Resolver<T>[] = [];
 
-  then(resolve: Resolver<T>) {
+  async then(resolve: Resolver<T>): Promise<T> {
     this._awaiters.push(resolve);
+    return new Promise<T>((localResolve) => this._awaiters.push(localResolve));
   }
 
   emit(value: T) {

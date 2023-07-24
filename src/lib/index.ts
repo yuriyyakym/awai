b68b35c60a8f79850dc65150a9ab742618823b56
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-07-24 13:40:13 +0700

    Remove composeState util

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 export { default as AwaitableEvent } from './AwaitableEvent';
-export { default as composeState } from './composeState';
 export { default as delay } from './delay';
 export { default as fork } from './fork';
 export { default as isFunction } from './isFunction';

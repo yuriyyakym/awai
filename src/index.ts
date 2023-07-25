1d0db4860ae53a853fcc36cb15b3e8bd21fe08af
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-07-25 16:11:06 +0700

    Export types

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,16 @@ export { AwaitableEvent, delay, fork, rejectAfter } from './lib';
 export { scenario, scenarioOnce, scenarioOnEvery } from './scenario';
 export { default as selector } from './selector';
 export { default as state, type State } from './state';
+export {
+  type AsyncSetter,
+  type AsyncStatus,
+  type AsyncValue,
+  type FamilyState,
+  type InferReadableType,
+  type ReadableAsyncState,
+  type ReadableState,
+  type Setter,
+  type WritableAsyncState,
+  type WritableState,
+  isReadableAsyncState,
+} from './types';

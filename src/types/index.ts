6a8ad891046bad47d2f3df32c44c91316bb08c11
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-10 03:27:52 +0700

    Add Id type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export { type default as AsyncSetter } from './AsyncSetter';
 export { default as AsyncStatus } from './AsyncStatus';
 export { type default as AsyncValue } from './AsyncValue';
 export { type default as FamilyState } from './FamilyState';
+export { type default as Id } from './Id';
 export { type default as InferReadableType } from './InferReadableType';
 export { type default as ReadableAsyncState, isReadableAsyncState } from './ReadableAsyncState';
 export { type default as ReadableState } from './ReadableState';

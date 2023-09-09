c606673c9807661279f73e74969404a77835a1df
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-10 04:50:53 +0700

    Refactor AwaitableEvent

--- src/core/AwaitableEvent.ts
+++ src/core/AwaitableEvent.ts
@@ -1,17 +1,23 @@
 import queue from '../global/queue';
-import { type Resolver } from '../types';
+import { isFunction } from '../lib';
 
+type Resolver<T> = (value: T) => any;
 type FilterPredicate<T> = (value: T) => boolean;
 
 export default class AwaitableEvent<T> implements PromiseLike<T> {
   private _awaiters: Resolver<T>[] = [];
 
   then: PromiseLike<T>['then'] = (onfulfilled) => {
-    if (onfulfilled) {
-      this._awaiters.push(onfulfilled as any);
+    if (!isFunction(onfulfilled)) {
+      return Promise.resolve<any>(undefined);
     }
 
-    return new Promise((localResolve) => this._awaiters.push(localResolve as any));
+    return new Promise<ReturnType<typeof onfulfilled>>((localResolve) => {
+      this._awaiters.push((value: T) => {
+        const result = onfulfilled(value);
+        localResolve(result);
+      });
+    });
   };
 
   emit(value: T) {
@@ -29,7 +35,7 @@ export default class AwaitableEvent<T> implements PromiseLike<T> {
   async filter(predicate: FilterPredicate<T>) {
     return new Promise(async (resolve) => {
       while (true) {
-        const newValue = (await this) as T;
+        const newValue = await this;
         const isApplicable = Boolean(predicate(newValue));
 
         if (isApplicable) {

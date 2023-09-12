fe5a2ede4fe4f1fc49f38a76e211205eafb9188e
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-13 02:10:41 +0700

    Refactor effect types

--- src/effect/types.ts
+++ src/effect/types.ts
@@ -1,18 +1,24 @@
 import { type AwaitableEvent } from '../core';
-import { InferReadableType } from '../types';
+import type { InferReadableType, ReadableAsyncState, ReadableState } from '../types';
 
 export type CleanupCallback = () => void;
 
-export interface RunEvent<T extends any[], V extends { [K in keyof T]: InferReadableType<T[K]> }> {
+export interface RunEvent<
+  T extends (ReadableState | ReadableAsyncState)[],
+  V extends { [K in keyof T]: InferReadableType<T[K]> },
+> {
   states: T;
   values: V;
 }
 
-export interface ClearedEvent<T extends any[]> {
+export interface ClearedEvent<T extends (ReadableState | ReadableAsyncState)[]> {
   states: T;
 }
 
-export interface Effect<T extends any[], V extends { [K in keyof T]: InferReadableType<T[K]> }> {
+export interface Effect<
+  T extends (ReadableState | ReadableAsyncState)[],
+  V extends { [K in keyof T]: InferReadableType<T[K]> },
+> {
   events: {
     cleared: AwaitableEvent<ClearedEvent<T>>;
     run: AwaitableEvent<RunEvent<T, V>>;

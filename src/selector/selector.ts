85da4e8ffe035f2dc9abab25161e0f7a768a48bc
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-14 20:58:32 +0700

    Make selectors configurable

--- src/selector/selector.ts
+++ src/selector/selector.ts
@@ -1,34 +1,23 @@
 import { isReadableAsyncState } from '../lib';
-import { type InferReadableType, type ReadableAsyncState } from '../types';
+import { type InferReadableType } from '../types';
 
 import asyncSelector from './asyncSelector';
 import syncSelector from './syncSelector';
-import type { AsyncSelector, SyncSelector } from './types';
-
-type ContainsAsync<T extends any[]> = T extends [infer First, ...infer Rest]
-  ? First extends ReadableAsyncState<any>
-    ? true
-    : ContainsAsync<Rest>
-  : false;
-
-type Return<T extends any[], U> = U extends PromiseLike<infer P>
-  ? AsyncSelector<P>
-  : ContainsAsync<T> extends true
-  ? AsyncSelector<U>
-  : SyncSelector<U>;
+import type { Config, Selector } from './types';
 
 const selector = <T extends any[], U>(
   states: [...T],
   predicate: (...values: { [K in keyof T]: InferReadableType<T[K]> }) => U,
-): Return<T, U> => {
+  customConfig: Partial<Config<T, U>>,
+): Selector<T, U> => {
   const isAsyncPredicate = predicate.constructor.name === 'AsyncFunction';
 
   const state =
     isAsyncPredicate || states.some((state) => isReadableAsyncState(state))
-      ? asyncSelector(states, predicate)
-      : syncSelector(states, predicate);
+      ? asyncSelector(states, predicate, customConfig)
+      : syncSelector(states, predicate, customConfig);
 
-  return state as Return<T, U>;
+  return state as Selector<T, U>;
 };
 
 export default selector;
--- src/selector/types.ts
+++ src/selector/types.ts
@@ -1,5 +1,31 @@
-import { type ReadableAsyncState, type ReadableState } from '../types';
+import type { BaseConfig, ReadableAsyncState, ReadableState } from '../types';
 
-export type SyncSelector<T> = ReadableState<T>;
+export type SyncConfig = BaseConfig;
 
-export type AsyncSelector<T> = ReadableAsyncState<T>;
+export type AsyncConfig = BaseConfig;
+
+export type SyncSelector<T> = ReadableState<T> & {
+  config: SyncConfig;
+};
+
+export type AsyncSelector<T> = ReadableAsyncState<T> & {
+  config: AsyncConfig;
+};
+
+export type ContainsAsync<T extends any[]> = T extends [infer First, ...infer Rest]
+  ? First extends ReadableAsyncState<any>
+    ? true
+    : ContainsAsync<Rest>
+  : false;
+
+export type Selector<T extends any[], U> = U extends PromiseLike<infer P>
+  ? AsyncSelector<P>
+  : ContainsAsync<T> extends true
+  ? AsyncSelector<U>
+  : SyncSelector<U>;
+
+export type Config<T extends any[], U> = U extends PromiseLike<any>
+  ? AsyncConfig
+  : ContainsAsync<T> extends true
+  ? AsyncConfig
+  : SyncConfig;

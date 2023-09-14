c8633a7a86afb3ff71404e0cb4162b0c2ef42b05
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-14 23:17:27 +0700

    Add scenario test cases

--- tests/scenario.test.ts
+++ tests/scenario.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, vi } from 'vitest';
 
-import { SystemTag, action, delay, flush, scenario } from '../src';
+import { SystemTag, action, delay, flush, rejectAfter, scenario } from '../src';
 
 test('runs scenario on event', async () => {
   const click = action();
@@ -17,20 +17,103 @@ test('runs scenario on event', async () => {
   expect(callback.mock.calls.length).toEqual(3);
 });
 
-test('handles cyclic strategies properly', async () => {
-  const click = action();
-  const callback = vi.fn(() => delay(10));
+test('handles trigger factory', async () => {
+  const onTick = vi.fn();
+  scenario(() => delay(10), onTick);
+  await delay(35);
+  expect(onTick.mock.calls.length).toEqual(3);
+});
 
-  scenario(click.events.invoked, callback, { strategy: 'cyclic' });
+test('handles strategies properly', async () => {
+  const click = action();
+
+  const cyclicCallback = vi.fn(() => delay(10));
+  const forkCallback = vi.fn(() => delay(10));
+  const onceCallback = vi.fn(() => delay(10));
+
+  scenario(click.events.invoked, cyclicCallback, { strategy: 'cyclic' });
+  scenario(click.events.invoked, forkCallback, { strategy: 'fork' });
+  scenario(click.events.invoked, onceCallback, { strategy: 'once' });
 
   click();
   click();
   await delay(30);
   click();
+  await delay(5);
+  click();
 
   await flush();
 
-  expect(callback.mock.calls.length).toEqual(2);
+  expect(cyclicCallback.mock.calls.length).toBe(2);
+  expect(forkCallback.mock.calls.length).toBe(4);
+  expect(onceCallback.mock.calls.length).toBe(1);
+});
+
+test('emits `started` event', async () => {
+  const click = action();
+  const callback = () => delay(10);
+
+  const cyclicCallback = vi.fn();
+  const forkCallback = vi.fn();
+  const onceCallback = vi.fn();
+
+  const cyclicScenario = scenario(click.events.invoked, callback, { strategy: 'cyclic' });
+  const forkScenario = scenario(click.events.invoked, callback, { strategy: 'fork' });
+  const onceScenario = scenario(click.events.invoked, callback, { strategy: 'once' });
+
+  scenario(cyclicScenario.events.started, cyclicCallback);
+  scenario(forkScenario.events.started, forkCallback);
+  scenario(onceScenario.events.started, onceCallback);
+
+  click();
+  click();
+  await delay(30);
+  click();
+  await delay(5);
+  click();
+
+  await flush();
+
+  expect(cyclicCallback.mock.calls.length).toBe(2);
+  expect(forkCallback.mock.calls.length).toBe(4);
+  expect(onceCallback.mock.calls.length).toBe(1);
+});
+
+test('emits `completed` event', async () => {
+  const click = action();
+  const onCompleted = vi.fn();
+
+  const testScenario = scenario(click.events.invoked, () => delay(10), { strategy: 'cyclic' });
+  scenario(testScenario.events.completed, onCompleted);
+
+  click();
+  click();
+  await delay(30);
+  click();
+  await delay(5);
+  click();
+  await delay(5);
+
+  await flush();
+
+  expect(onCompleted.mock.calls.length).toBe(2);
+});
+
+test('emits `failed` event', async () => {
+  const click = action();
+  const onFailure = vi.fn();
+
+  const testScenario = scenario(click.events.invoked, () => rejectAfter(5), {
+    strategy: 'cyclic',
+  });
+
+  scenario(testScenario.events.failed, onFailure);
+
+  click();
+
+  await delay(10);
+
+  expect(onFailure.mock.calls.length).toBe(1);
 });
 
 test('automatically assigns id when not provided', () => {

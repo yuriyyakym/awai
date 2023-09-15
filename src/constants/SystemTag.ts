45d0d4600309c2ce9f8de8fd5e0a56711e4eb3ec
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-09-15 17:35:18 +0700

    Update SystemTag enum

--- src/constants/SystemTag.ts
+++ src/constants/SystemTag.ts
@@ -1,12 +1,13 @@
 enum SystemTag {
-  ACTION = 'action',
-  ASYNC_SELECTOR = 'async-selector',
-  ASYNC_STATE = 'async-state',
-  EFFECT = 'effect',
-  FAMILY_STATE = 'family-state',
-  SCENARIO = 'scenario',
-  SELECTOR = 'selector',
-  STATE = 'state',
+  ACTION = '@awai/action',
+  ASYNC_SELECTOR = '@awai/async-selector',
+  ASYNC_STATE = '@awai/async-state',
+  CORE_NODE = '@awai/core-node',
+  EFFECT = '@awai/effect',
+  FAMILY_STATE = '@awai/family-state',
+  SCENARIO = '@awai/scenario',
+  SELECTOR = '@awai/selector',
+  STATE = '@awai/state',
 }
 
 export default SystemTag;

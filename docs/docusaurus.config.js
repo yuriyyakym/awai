ce8da7cfc6be8e68e7dfba13e909c14752f85cd9
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-11 06:15:59 +0700

    Add docs favicon and logo

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,8 @@
 const config = {
   title: 'Awai docs',
   tagline: 'Dependency-free state management library written in TypeScript',
-  // favicon: 'img/favicon.ico',
+  favicon: '/logo.svg',
+  staticDirectories: ['static'],
   url: 'https://...',
   baseUrl: '/',
   organizationName: 'yuriyyakym',
@@ -12,6 +13,18 @@ const config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
+  },
+  themeConfig: {
+    navbar: {
+      title: 'Awai',
+      logo: {
+        alt: 'Awai docs',
+        src: '/logo.svg',
+        target: '_self',
+        width: 32,
+        height: 32,
+      },
+    },
   },
   plugins: [require.resolve('docusaurus-lunr-search')],
   presets: [

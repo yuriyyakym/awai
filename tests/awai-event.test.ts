bda1db05ce565839b81522b650fdab0d64156805
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-12-03 03:55:01 +0700

    Add AwaiEvent tests
    Reach 100% coverage
    Closes https://github.com/yuriyyakym/awai/issues/38

--- tests/awai-event.test.ts
+++ tests/awai-event.test.ts
@@ -1,6 +1,83 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 
-import { AwaiEvent } from '../src';
+import { AwaiEvent, delay } from '../src';
+
+test('registers awaiter and resolves it when emitted', () => {
+  const event = new AwaiEvent<string>();
+  expect(event).resolves.toBe('Awai');
+  event.emit('Awai');
+});
+
+test('registers multiple awaiters and resolves them when emitted', async () => {
+  const event = new AwaiEvent<string>();
+  expect(event).resolves.toBe('Awai');
+  expect(event).resolves.toBe('Awai');
+  expect(Promise.resolve(event)).resolves.toBe('Awai');
+  expect(Promise.all([event, Promise.resolve(event)])).resolves.toEqual(['Awai', 'Awai']);
+  setTimeout(() => event.emit('Awai'), 0);
+});
+
+test('handles re-subscriptions and emits new values', async () => {
+  const event = new AwaiEvent<string>();
+  setTimeout(() => event.emit('Awai'), 0);
+  await expect(event).resolves.toBe('Awai');
+  setTimeout(() => event.emit('Hello Awai'), 0);
+  await expect(event).resolves.toBe('Hello Awai');
+});
+
+test('returns promise even if no callback is passed to `then` method', async () => {
+  const event = new AwaiEvent<string>();
+  expect(event.then()).resolves.toBe('Awai');
+  event.emit('Awai');
+});
+
+test('rejects when aborted', async () => {
+  const event = new AwaiEvent<string>();
+  const abortController = new AbortController();
+  expect(event.abortable(abortController)).rejects.toBe('Aborted');
+  await delay(0);
+  abortController.abort();
+});
+
+test('removes callback from awaiters when aborted', async () => {
+  const resolve = vi.fn();
+  const reject = vi.fn();
+  const event = new AwaiEvent();
+  const abortController = new AbortController();
+  event.then(resolve);
+  event.abortable(abortController).catch(reject);
+  abortController.abort();
+  event.emit();
+  await delay(0);
+  expect(resolve).toBeCalled();
+  expect(reject).toBeCalled();
+});
+
+test('rejects when callback throws', async () => {
+  const event = new AwaiEvent();
+
+  const promise = event.then(() => {
+    throw 'Some error';
+  });
+  const wrappedPromise = Promise.resolve(promise);
+
+  expect(promise).rejects.toBe('Some error');
+  expect(wrappedPromise).rejects.toBe('Some error');
+
+  event.emit();
+});
+
+test('rejects when callback returns rejected promise', async () => {
+  const event = new AwaiEvent();
+
+  const promise = event.then(() => Promise.reject('Some error'));
+  const wrappedPromise = Promise.resolve(promise);
+
+  expect(promise).rejects.toBe('Some error');
+  expect(wrappedPromise).rejects.toBe('Some error');
+
+  event.emit();
+});
 
 test('filters out non-applicable events', async () => {
   const number = new AwaiEvent<number>();

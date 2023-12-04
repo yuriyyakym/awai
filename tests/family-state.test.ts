083e88ae2709c7eb4052b37d27671b2c4ce0c26d
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-12-05 01:22:37 +0700

    Add & refactor family-state tests
    Reach 100% coverage
    Closes https://github.com/yuriyyakym/awai/issues/40

--- tests/family-state.test.ts
+++ tests/family-state.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, vi } from 'vitest';
 
-import { SystemTag, delay, familyState, scenario } from '../src';
+import { SystemTag, delay, familyState, flush, scenario, state } from '../src';
 
 test('creates states using initializer', async () => {
   const family = familyState((id) => `${id}-test`);
@@ -8,6 +8,8 @@ test('creates states using initializer', async () => {
   const node1 = family.getNode('1');
   const node2 = family.getNode('2');
 
+  expect(node1.config.tags).toContain(SystemTag.STATE);
+  expect(node2.config.tags).toContain(SystemTag.STATE);
   expect(node1.get()).toEqual('1-test');
   expect(node2.get()).toEqual('2-test');
 });
@@ -21,30 +23,68 @@ test('is thennable', async () => {
 
 test('creates async states using initializer', async () => {
   const family = familyState((id) => delay(30).then(() => `${id}-test`));
-
   const node1 = family.getNode('1');
   const node2 = family.getNode('2');
 
-  await Promise.all([node1.events.changed, node2.events.changed]);
-
+  expect(node1.config.tags).toContain(SystemTag.ASYNC_STATE);
+  expect(node2.config.tags).toContain(SystemTag.ASYNC_STATE);
+  expect(node1.events.fulfilled).resolves.toEqual(`1-test`);
+  await Promise.all([node1.events.fulfilled, node2.events.fulfilled]);
   expect(node1.get()).toEqual('1-test');
   expect(node2.get()).toEqual('2-test');
 });
 
-test('emits change event when node added', async () => {
-  const family = familyState((id) => delay(30).then(() => `${id}-test`));
+test('emits `changed` event when node created by `getNode` method', async () => {
   const onFamilyChanged = vi.fn();
-
+  const family = familyState((id) => `${id}-test`);
   scenario(family.events.changed, onFamilyChanged);
+  family.getNode('1');
+  family.getNode('2');
+  await flush();
+  expect(onFamilyChanged.mock.calls.length).toBe(2);
+});
 
+test('emits `changed` event when node set', async () => {
+  const onFamilyChanged = vi.fn();
+  const family = familyState((id) => `${id}-test`);
+  scenario(family.events.changed, onFamilyChanged);
+  family.setNode('test', state('test'));
+  await flush();
+  expect(onFamilyChanged.mock.calls.length).toBe(1);
+  family.setNode('test2', state('test2'));
+  await flush();
+  expect(onFamilyChanged.mock.calls.length).toBe(2);
+});
+
+test('emits `stateCreated` once per requested key', async () => {
+  const onNodeCreated = vi.fn();
+  const family = familyState((id) => `${id}-test`);
+  scenario(family.events.changed, onNodeCreated);
+  family.getNode('1');
+  await flush();
+  expect(onNodeCreated.mock.calls.length).toBe(1);
+  family.getNode('2');
+  await flush();
+  expect(onNodeCreated.mock.calls.length).toBe(2);
+  family.getNode('1');
+  await flush();
+  expect(onNodeCreated.mock.calls.length).toBe(2);
+});
+
+test('emits `changed` event when any node changed', async () => {
+  const onFamilyChanged = vi.fn();
+  const family = familyState((id) => delay(30).then(() => `${id}-test`));
   const node1 = family.getNode('1');
   const node2 = family.getNode('2');
-  await delay(20);
+  await Promise.all([node1.events.changed, node2.events.changed]);
+  await flush();
+  scenario(family.events.changed, onFamilyChanged);
   await node1.set('3');
-  await delay(20);
+  await flush();
+  expect(onFamilyChanged.mock.calls.length).toBe(1);
   await node2.set('4');
-
-  expect(onFamilyChanged.mock.calls.length).toBe(5);
+  await flush();
+  expect(onFamilyChanged.mock.calls.length).toBe(2);
 });
 
 test('returns same node for the same key', async () => {

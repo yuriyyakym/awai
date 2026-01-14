a426db45fb4990bf378beaf61e4281a9eb3a51f7
Yuriy Yakym <yakym.yuriy@gmail.com>
2026-01-15 05:02:50 +0700

    Resolve scenario expiration promise right away if signal is aborted

--- src/scenario/lib/getExpirationPromise.ts
+++ src/scenario/lib/getExpirationPromise.ts
@@ -2,6 +2,10 @@ import type { ShouldExpirePredicate, UntilTrigger } from '../types';
 
 const getExpirationPromise = <E>(until: Exclude<UntilTrigger<E>, ShouldExpirePredicate>) => {
   if (typeof AbortSignal !== 'undefined' && until instanceof AbortSignal) {
+    if (until.aborted) {
+      return Promise.resolve(undefined);
+    }
+
     return new Promise((resolve) => {
       until.addEventListener('abort', () => resolve(undefined), { once: true });
     });
